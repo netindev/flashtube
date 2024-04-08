183dc359746aa2234e34131aed4b2f9e1ea016bd
Thiago Barbieri Catarina <tcatarina@proton.me>
2024-04-08 17:20:26 -0300

    Add DownloadVideo method and basic download operation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,18 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using flashtube.Models;
+using YoutubeDLSharp;
+using System.Net;
+using System.Net.Mime;
 
 namespace flashtube.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
-        return View();
+        YoutubeLinkModel video = new YoutubeLinkModel(); 
+        return View(video);
     }
 
     public IActionResult Privacy()
@@ -23,16 +20,39 @@ public class HomeController : Controller
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [Route("DownloadVideo")]
+    public async Task<IActionResult> DownloadVideo(string Url)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        if (string.IsNullOrEmpty(Url))
+        {
+            return BadRequest("Please provide a valid video URL.");
+        }
+
+        try
+        {
+            var ytdl = new YoutubeDL();
+            ytdl.YoutubeDLPath = "C:/Users/thiago.barbieri/Documents/Projects/flashtube/bin/yt-dlp.exe";
+            ytdl.FFmpegPath = "C:/Users/thiago.barbieri/Documents/Projects/flashtube/bin/ffmpeg.exe";
+            var downloadResult = await ytdl.RunVideoDownload(Url);
+
+            if (downloadResult.Success)
+            {
+                var filePath = downloadResult.Data;
+                var fileName = Path.GetFileName(filePath);
+
+                var fileBytes = System.IO.File.ReadAllBytes(filePath);
+                var mimeTypes = MimeTypes.GetMimeType(filePath);
+                return File(fileBytes, mimeTypes, fileName);
+            }
+            else
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, downloadResult.ErrorOutput);
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+        }
     }
 
-    public IActionResult FireAction(string youtubeLink)
-    {
-        _logger.LogInformation("Received a download link: " + youtubeLink);
-
-        return Content(youtubeLink);
-    }
 }
